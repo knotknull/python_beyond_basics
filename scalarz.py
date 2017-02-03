5c77e12ec287c2edf987ba7552ff66df01bb8ab1
Marc Pulido <marc.pulido@gmail.com>
2017-02-02 23:44:36 -0500

    Added scalarz.py

---
+++ scalarz.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.5
+
+# scalar types:
+# int -- arbitrary precision integer
+# float -- 64 bit floating point number
+# NoneType = None = the null object
+# bool = True | False : boolean logic values
