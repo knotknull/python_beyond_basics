99de778ea2496c8760fca6e46248894e6e0cfc06
Marc Pulido <marc.pulido@gmail.com>
2017-02-06 23:18:02 -0500

    added listz.py and dictz.py

---
+++ dictz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.5
+
+# dict : mutable mappings of keys to values
+
+# literal dictionaly
+name_nums = {'joe': 5,
+             'bob': 12,
+             'infy': float("inf"),
+             }
+
+print("bob = ", name_nums['bob'])
+print("infy = ", name_nums['infy'])
--- listz.py
+++ listz.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3.5
 
-# bytes : immutable sequences of bytes
+# list : mutable sequences of bytes
+num_lst = [1, 2, 5]
+str_lst = ['apple', 'orange', 'cherry']
+mixed_lst = [1, 'apple', None]
 
-# bytes literals
-byt1 = b'data'
-byt2 = b"datas"
-print(byt1)
-print(byt2)
+# print entire list
+print("num_lst = ", num_lst)
+print("str_lst = ", str_lst)
+print("mixed_lst = ", mixed_lst)
+
+# print list element
+print("num_lst[1] = ", num_lst[1])
+print("str_lst[1] = ", str_lst[1])
+print("mixed_lst[1] = ", mixed_lst[1])
